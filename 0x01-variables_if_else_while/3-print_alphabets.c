11e632eff7a1e2fdf24f7bdd2affff4abc6aca87
Onomah <onomahturaki01@gmail.com>
2023-02-20 07:26:42 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,15 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = "a"; ch <= "z"; ch++)
 		putchar(ch);
 
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		purchar(ch);
+	for (ch = "A"; ch <= "Z"; ch++)
+		putchar(ch);
 
-	purchar('\n');
+	putchar("\n");
 
 	return (0);
 }
+
+
